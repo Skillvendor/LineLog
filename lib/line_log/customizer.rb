50572be1b58d27afbd83ba9597e844ecee6749ac
Skillvendor <skillvendor193@gmail.com>
2018-03-13 15:40:57 +0200

    no need for methods

--- lib/line_log/customizer.rb
+++ lib/line_log/customizer.rb
@@ -6,7 +6,7 @@ module LineLog
     def initialize(app, logger=nil, formatter=LineLog::Formatters::KeyValue.new)
       @app = app
       @logger = logger
-      set_formatter(formatter)
+      @formatter = formatter
     end
 
     # making it thread safe. Visit https://github.com/cerner/gc_stats/issues/3 for more info about this
@@ -19,14 +19,6 @@ module LineLog
       @status, @headers, @response = @app.call(event)
 
       LineLog::Writer.call(event, @status, began_at, @logger)
-    end
-
-    def options=(options)
-      LineLog::Customizer.options = options 
-    end
-
-    def formatter=(formatter)
-      LineLog::Customizer.formatter = formatter
     end
   end
 end
