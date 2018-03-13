9401c861759dd2c3abf810fc8a9efead3df9bc73
Skillvendor <skillvendor193@gmail.com>
2018-03-13 13:19:32 +0200

    added customizer to the mix

---
+++ lib/line_log/customizer.rb
@@ -0,0 +1,23 @@
+class LineLog::Customizer
+  mattr_accessor :options
+  mattr_accessor :formatter
+  
+  def initialize(app, logger=nil, formatter=nil)
+    @app = app
+    @logger = logger
+    @formatter = formatter
+  end
+
+  # making it thread safe. Visit https://github.com/cerner/gc_stats/issues/3 for more info about this
+  def call(event)
+    dup._call(event)
+  end
+
+  def _call(event)
+    began_at = Time.now
+    @status, @headers, @response = @app.call(event)
+
+    binding.remote_pry
+    Writer.log(event, @status, began_at, @logger)
+  end
+end
--- lib/line_log/formatters/key_value.rb
+++ lib/line_log/formatters/key_value.rb
@@ -1,27 +1,23 @@
-class LineLog
-  module Formatters
-    class KeyValue
-      def call(data)
-        data.keys
-          .map { |key| format(key, data[key]) }
-          .join(' ')
-      end
+class LineLog::Formatters::KeyValue
+  def call(data)
+    data.keys
+      .map { |key| format(key, data[key]) }
+      .join(' ')
+  end
 
-      protected
+  protected
 
-      def format(key, value)
-        "#{key}=#{parse_value(key, value)}"
-      end
+  def format(key, value)
+    "#{key}=#{parse_value(key, value)}"
+  end
 
-      def parse_value(key, value)
-        # Exactly preserve the previous output
-        # Parsing this can be ambigious if the error messages contains
-        # a single quote
-        return "'#{value}'" if key == :error
-        return Kernel.format('%.2f', value) if value.is_a? Float
+  def parse_value(key, value)
+    # Exactly preserve the previous output
+    # Parsing this can be ambigious if the error messages contains
+    # a single quote
+    return "'#{value}'" if key == :error
+    return Kernel.format('%.2f', value) if value.is_a? Float
 
-        value
-      end
-    end
+    value
   end
 end
