bee1080273c710be2cb402b190aa8172c2c1ddd9
Skillvendor <skillvendor193@gmail.com>
2018-03-14 13:06:29 +0200

    deleted unused comment

--- lib/line_log/writer.rb
+++ lib/line_log/writer.rb
@@ -3,8 +3,6 @@ module LineLog
     def self.call(message_builder=LineLog::MessageBuilder.new, logger)
       formatted_message = message_builder.call
       
-      # Standard library logger doesn't support write but it supports << which actually
-      # calls to write on the log device without formatting
       if logger.respond_to?(:write)
         logger.write(formatted_message)
       else
