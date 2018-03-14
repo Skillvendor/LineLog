c40225a31693fa06fdf11b0ae253cf3a01c9bd54
Skillvendor <skillvendor193@gmail.com>
2018-03-14 14:53:12 +0200

    removed pry

--- lib/line_log.rb
+++ lib/line_log.rb
@@ -4,8 +4,6 @@ require 'line_log/request_data_extractor'
 require 'line_log/formatters/key_value'
 require 'line_log/customizer'
 require 'line_log/message_builder'
-require 'pry'
-
 
 module LineLog
   
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'line_log'
 require 'time'
-require 'pry'
 
-def fixtures_path 
+def fixtures_path
   "#{File.dirname(File.expand_path(__FILE__))}/fixtures"
 end
